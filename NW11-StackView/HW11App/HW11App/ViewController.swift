f3f6e0a44d23c9d9074db6be8f8e329ccd81504c
Danila Kokin <dakokin@yandex.ru>
2024-07-08 18:25:32 +0200

    hw11 uploaded

---
+++ NW11-StackView/HW11App/HW11App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW11App
+//
+//  Created by Danila Kokin on 08.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
